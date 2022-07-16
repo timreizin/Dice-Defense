92a630d72da2621a8bff506eb58eabc4596a07da
anton <kamanton3@gmail.com>
2022-07-16 19:45:32 +0200

    dice is ready

--- Assets/Scripts/BackgroundManagment.cs
+++ Assets/Scripts/BackgroundManagment.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class BackgroundManagment : MonoBehaviour
 {
     public GameObject backgroundArrows;
+    public GameObject backgroundYourTurnLabel;
+    public GameObject BackgroundRightFace;
+    public GameObject BackgroundLeftFace;
+    public GameObject BackgroundTopFace;
+    public GameObject BackgroundUpFace;
+    public GameObject BackgroundDownFace;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         backgroundArrows.SetActive(false);
+        backgroundYourTurnLabel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,9 +36,26 @@ public class BackgroundManagment : MonoBehaviour
             GlobalGameData.IsTabMenuOpened = false;
         }
 
-        //check for game pause
-        {
+        //handle
 
+        if(GlobalGameData.gamePhase == "playerTurn")
+        {
+            backgroundYourTurnLabel.SetActive(true);
         }
+        else
+        {
+            backgroundYourTurnLabel.SetActive(false);
+        }
+
+        //check for game pause
+        
+    }
+    public void updateBackgroundPlayerFaces()
+    {
+        BackgroundRightFace.GetComponent<SpriteRenderer>().sprite = player.GetComponent<PlayerManagment>().rightFace.sprite;
+        BackgroundLeftFace.GetComponent<SpriteRenderer>().sprite = player.GetComponent<PlayerManagment>().leftFace.sprite;
+        BackgroundUpFace.GetComponent<SpriteRenderer>().sprite = player.GetComponent<PlayerManagment>().upFace.sprite;
+        BackgroundDownFace.GetComponent<SpriteRenderer>().sprite = player.GetComponent<PlayerManagment>().downFace.sprite;
+        BackgroundTopFace.GetComponent<SpriteRenderer>().sprite = player.GetComponent<PlayerManagment>().topFace.sprite;
     }
 }
